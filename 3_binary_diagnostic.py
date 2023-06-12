21f63da7f3cdacd0a8ed4aa65d4e0c9845354547
Peafly78 <90190882+Peafly78@users.noreply.github.com>
2023-06-12 21:48:38 +0200

    calculate oxygen generator rating

--- 3_binary_diagnostic.py
+++ 3_binary_diagnostic.py
@@ -36,3 +36,37 @@ epsilon_rate = "".join(["0" if binary == "1" else "1" for binary in gamma_rate])
 result = int(gamma_rate, 2) * int(epsilon_rate, 2)
 print(result)
 
+
+#****************** Part 1 *****
+
+
+# determine most common bit
+
+def find_most_common_bit(array, position):
+    for i in range(position-1, position):
+        temp_binary = ""
+        for binary in array:
+            temp_binary += binary[i]
+        zero_count = temp_binary.count("0")
+        one_count = temp_binary.count("1")
+        most_common = max(zero_count, one_count)
+        if zero_count == one_count or most_common == one_count:
+            return "1"
+        else:
+            return "0"
+
+# filter binary numbers
+
+def filter_binary_list_for_most_common(array):
+    for i in range(len(array[0])):
+        if len(array) == 1:
+            return array[0]
+        keep = find_most_common_bit(array, i+1)
+        array = [binary for binary in array if binary[i] == keep]
+    return array[0]
+        
+# calculate oxygen generator rating
+
+oxygen_generator_rating = filter_binary_list_for_most_common(import_input_data)
+
+print(oxygen_generator_rating)
