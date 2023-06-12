5b17f6dca8ecd5d5db46017c542161001a8fcaf6
Peafly78 <90190882+Peafly78@users.noreply.github.com>
2023-06-12 20:12:48 +0200

    adapt calculation for part 2

--- 2_dive.py
+++ 2_dive.py
@@ -34,3 +34,27 @@ result = submarine_pos[0] * submarine_pos[1]
 
 print(f"\nThe new submarine position is {submarine_pos} and the resulting calculation is {result}.\n")
 
+
+#****************** Part 2 *****
+
+
+# adapt position calculation
+
+submarine_data = [0, 0, 0]
+for instr in instructions:
+    if instr[0] == "f":
+        submarine_data[0] += instr[1]
+        submarine_data[1] += instr[1] * submarine_data[2]
+    elif instr[0] == "d":
+        submarine_data[2] += instr[1]
+    elif instr[0] == "u":
+        submarine_data[2] -= instr[1]
+    else:
+        print("Sorry, cannot carry out instruction")
+
+result_2 = submarine_data[0] * submarine_data[1]
+
+# output result
+
+print(f"\nThe new submarine position is {submarine_data[:2]} and the resulting calculation is {result_2}.\n")
+
