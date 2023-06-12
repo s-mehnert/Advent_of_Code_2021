93ad99075254a25b26d690064c436eee62e54f76
Peafly78 <90190882+Peafly78@users.noreply.github.com>
2023-06-12 20:47:43 +0200

    import binary data

---
+++ 3_binary_diagnostic.py
@@ -0,0 +1,13 @@
+#***** ADVENT OF CODE 2021 *****
+#************ DAY 3 ************
+#****************** Part 1 *****
+
+
+# get input data
+
+import_input_data = list()
+
+with open("3_binary_diagnostic_input.txt") as input:
+    for line in input.readlines():
+        import_input_data.append(line.strip("\n"))
+
