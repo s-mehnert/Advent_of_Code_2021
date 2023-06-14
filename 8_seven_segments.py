51efde31ae5ffaa10a318e74ccce4dc4c2dde20f
Peafly78 <90190882+Peafly78@users.noreply.github.com>
2023-06-14 09:10:16 +0200

    Day 8: import and format test data

---
+++ 8_seven_segments.py
@@ -0,0 +1,24 @@
+#***** ADVENT OF CODE 2021 *****
+#************ DAY 8 ************
+#****************** Part 1 *****
+
+
+# get input data
+
+import_input_data = list()
+
+with open("8_seven_segments_input.txt") as input:
+    for line in input.readlines():
+        import_input_data.append(line.strip("\n").split(" | "))
+
+print(import_input_data)
+for line in import_input_data:
+    print(line)
+
+output_digits = [entry[1].split() for entry in import_input_data]
+
+print(output_digits)
+for output in output_digits:
+    print(output)
+
+
