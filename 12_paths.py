ccad78e3697ae2f84f417090f619694bb561cfbd
Peafly78 <90190882+Peafly78@users.noreply.github.com>
2023-06-15 22:34:19 +0200

    Day 12: import data and create outline

---
+++ 12_paths.py
@@ -0,0 +1,25 @@
+#***** ADVENT OF CODE 2021 *****
+#************ DAY 12 ***********
+#****************** Part 1 *****
+
+
+# get input data
+
+import_input_data = list()
+
+with open("12_paths_input.txt") as input:
+    for line in input.readlines():
+        import_input_data.append(line.strip("\n"))
+
+print(import_input_data)
+print()
+for line in import_input_data:
+    print(line)
+
+
+# create graph with caves as vertices and edges for the connections between them
+
+# implement algorithm to find all paths from start to end
+
+
+
