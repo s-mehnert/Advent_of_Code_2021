7d27d7a8d8215d338029209bd9ee68a6f99cbcd1
Peafly78 <90190882+Peafly78@users.noreply.github.com>
2023-06-15 22:45:33 +0200

    create vertex and graph classes

--- 12_paths.py
+++ 12_paths.py
@@ -19,6 +19,33 @@ for line in import_input_data:
 
 # create graph with caves as vertices and edges for the connections between them
 
+class Vertex:
+    def __init__(value):
+        self.value = value
+        self.edges = list()
+    
+    def add_edge(self, vertex):
+        self.edges.append(vertex)
+
+    def get_edges(self):
+        return self.edges
+    
+class Graph: 
+    def __init__(self):
+        self.graph = dict()
+    
+    def add_vertex(self, vertex):
+        self.graph_dict[vertex.value] = vertex
+    
+    def add_edge(self, from_vertex, to_vertex):
+        self.graph_dict[from_vertex.value].add_edge(to_vertex)
+        self.graph_dict[to_vertex.value].add_edge(from_vertex)
+    
+    # find all paths method
+    
+    def find_all_paths(self):
+        pass
+
 # implement algorithm to find all paths from start to end
 
 
