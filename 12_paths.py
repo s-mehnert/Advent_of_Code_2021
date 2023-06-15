bee5cceca950ff9e738f7dacfa61c93c2c15ca1a
Peafly78 <90190882+Peafly78@users.noreply.github.com>
2023-06-15 23:08:08 +0200

    prepare input data for import into graph

--- 12_paths.py
+++ 12_paths.py
@@ -9,7 +9,7 @@ import_input_data = list()
 
 with open("12_paths_input.txt") as input:
     for line in input.readlines():
-        import_input_data.append(line.strip("\n"))
+        import_input_data.append(line.strip("\n").split("-"))
 
 print(import_input_data)
 print()
@@ -45,6 +45,26 @@ class Graph:
     
     def find_all_paths(self):
         pass
+
+
+# import input data into graph
+
+caves = list()
+connections = dict()
+
+for entry in import_input_data:
+    for cave in entry:
+        if cave not in caves:
+            caves.append(cave)
+    if entry[0] not in connections:
+        connections[entry[0]] = [entry[1]]
+    else:
+        if entry[1] not in connections[entry[0]]:
+                connections[entry[0]].append(entry[1])
+            
+print()
+print(caves)
+print(connections)
 
 # implement algorithm to find all paths from start to end
 
