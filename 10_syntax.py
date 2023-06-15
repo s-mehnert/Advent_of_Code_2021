609ccd58a934790c5b5f61d9f6e56c43b50f5a63
Peafly78 <90190882+Peafly78@users.noreply.github.com>
2023-06-15 09:04:12 +0200

    simulate stack to check for corrupted lines in syntax

--- 10_syntax.py
+++ 10_syntax.py
@@ -11,6 +11,23 @@ with open("10_syntax_input.txt") as input:
     for line in input.readlines():
         import_input_data.append(line.strip("\n"))
 
-print(import_input_data)
-for line in import_input_data:
-    print(line)
+opening_chunk = ["(", "[", "{", "<"]
+closing_chunk = [")", "]", "}", ">"]
+pairs = ["()", "[]", "{}", "<>"]
+
+errors_found = list()
+
+# simulate stack
+
+check_stack = list()
+
+for char in import_input_data[0]:
+    if char in opening_chunk:
+        check_stack.append(char)
+    elif check_stack[-1] + char in pairs:
+        check_stack.pop()
+    else:
+        print("Line corrupted. Wrong closing bracket found:", char)
+        errors_found.append(char)
+
+print(check_stack)
