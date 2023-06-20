6add832d53507162a11b04219cb4bdf401e0f967
Peafly78 <90190882+Peafly78@users.noreply.github.com>
2023-06-20 14:39:09 +0200

    decode literal string

--- 16_decoder.py
+++ 16_decoder.py
@@ -15,5 +15,36 @@ binary_number = bin(int(hexadecimal_string, 16))
 
 print(binary_number)
 
+binary_string = str(binary_number)[2:]
 
+print(binary_string)
+print(len(binary_string))
+
+# create helper functions
+
+
+
+# decode the packets
+
+version = binary_string[:3]
+type = binary_string[3:6]
+
+rest_of_string = binary_string[6:]
+literal = ""
+if type == "100":
+    next_packet = rest_of_string[:5]
+    print("next", next_packet)
+    literal += next_packet[1:]
+    rest_of_string = rest_of_string[5:]
+    while next_packet[0] == "1":
+        next_packet = rest_of_string[:5]
+        print("still next", next_packet)
+        literal += next_packet[1:]
+        rest_of_string = rest_of_string[5:]
+    else:
+        rest_of_string = rest_of_string[5:]
+
+print(literal)
+int_result = int(literal, 2)
+print(int_result)
 
