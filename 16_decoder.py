02cbd96e722e0ddf35ac8a7b4349b45623499339
Peafly78 <90190882+Peafly78@users.noreply.github.com>
2023-06-20 14:18:06 +0200

    import data and convert string into binary number

---
+++ 16_decoder.py
@@ -0,0 +1,19 @@
+#***** ADVENT OF CODE 2021 *****
+#************ DAY 16 ***********
+#****************** Part 1 *****
+
+
+# get input data
+
+hexadecimal_string = "D2FE28"
+
+hexadecimal_number = hex(int(hexadecimal_string, 16))
+
+print(hexadecimal_number)
+
+binary_number = bin(int(hexadecimal_string, 16))
+
+print(binary_number)
+
+
+
