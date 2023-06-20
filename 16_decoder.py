f32d85f3384fa74f245d977b36b24630b9ed0ec7
Peafly78 <90190882+Peafly78@users.noreply.github.com>
2023-06-20 15:38:47 +0200

    create helper functions to decode message and construct versions list

--- 16_decoder.py
+++ 16_decoder.py
@@ -5,46 +5,116 @@
 
 # get input data
 
-hexadecimal_string = "D2FE28"
+hexadecimal_string_old1 = "D2FE28"
+hexadecimal_string = "38006F45291200"
 
-hexadecimal_number = hex(int(hexadecimal_string, 16))
+dec_dict = {
+    "0" : "0000",
+    "1" : "0001",
+    "2" : "0010",
+    "3" : "0011",
+    "4" : "0100",
+    "5" : "0101",
+    "6" : "0110",
+    "7" : "0111",
+    "8" : "1000",
+    "9" : "1001",
+    "A" : "1010",
+    "B" : "1011",
+    "C" : "1100",
+    "D" : "1101",
+    "E" : "1110",
+    "F" : "1111"
+    }
 
-print(hexadecimal_number)
 
-binary_number = bin(int(hexadecimal_string, 16))
-
-print(binary_number)
-
-binary_string = str(binary_number)[2:]
+binary_string = ""
+for char in hexadecimal_string:
+    binary_string += dec_dict[char]
 
 print(binary_string)
 print(len(binary_string))
 
 # create helper functions
 
+def decode_header(binary_string):
+    version = binary_string[:3]
+    type_id = binary_string[3:6]
+    rest_of_string = binary_string[6:]
+    return version, type_id, rest_of_string
 
-
-# decode the packets
-
-version = binary_string[:3]
-type = binary_string[3:6]
-
-rest_of_string = binary_string[6:]
-literal = ""
-if type == "100":
+def decode_literal(rest_of_string):
     next_packet = rest_of_string[:5]
-    print("next", next_packet)
-    literal += next_packet[1:]
+    literal = next_packet[1:]
     rest_of_string = rest_of_string[5:]
     while next_packet[0] == "1":
         next_packet = rest_of_string[:5]
-        print("still next", next_packet)
         literal += next_packet[1:]
         rest_of_string = rest_of_string[5:]
     else:
         rest_of_string = rest_of_string[5:]
+    return literal, rest_of_string
 
-print(literal)
-int_result = int(literal, 2)
-print(int_result)
+def decode_packet(binary_string):
+    packet_list = list()
+    version, type_id, rest_of_string = decode_header(binary_string)
+    packet_list.append(int(version, 2))
+    length_type = None
+    literal = ""
+    subpackets = ""
+    if type_id == "100":
+        literal, rest_of_string = decode_literal(rest_of_string)
+        return packet_list
+    else: # all other types of packets are operator packets
+        length_type = rest_of_string[0]
+        if length_type == "0":
+            length_in_bits = int(rest_of_string[1:16], 2)
+            rest_of_string = rest_of_string[16:]
+            subpackets = rest_of_string[:length_in_bits]
+            rest_of_string = rest_of_string[length_in_bits]
+            return packet_list + decode_packet(subpackets) # make it work for more than one subpackets (if first is literal)
+        else:
+            num_of_packets = rest_of_string[1:12]
+            rest_of_string = rest_of_string[12:]
+            subpackets = rest_of_string
+            return packet_list + decode_packet(rest_of_string)
+        
+packet_list = decode_packet(binary_string)
 
+print()
+print(packet_list)
+
+# # decode the packets
+
+# packet_list = list()
+
+# version, type_id, rest_of_string = decode_header(binary_string)
+# packet_list.append(int(version, 2))
+
+# literal = ""
+# length_type = None
+
+# subpackets = ""
+
+# if type_id == "100":
+#     literal, rest_of_string = decode_literal(rest_of_string)
+
+# else: # all other types of packets are operator packets
+#     length_type = rest_of_string[0]
+#     if length_type == "0":
+#         length_in_bits = int(rest_of_string[1:16], 2)
+#         rest_of_string = rest_of_string[16:]
+#         subpackets = rest_of_string[:length_in_bits]
+#         rest_of_string = rest_of_string[length_in_bits]
+#     else:
+#         num_of_packets = rest_of_string[1:12]
+#         rest_of_string = rest_of_string[12:]
+#         subpackets = rest_of_string
+
+
+# print("Version:", version)
+# print("Type:", type_id)
+# print("Length Id:", length_type)
+# print("Subpackets:", subpackets)
+# print("Literal:", literal)
+# print("Rest of string:", rest_of_string)
