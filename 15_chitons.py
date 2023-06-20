4ae956595c2e032e6a9398a07bb8da265b1441e5
Peafly78 <90190882+Peafly78@users.noreply.github.com>
2023-06-20 10:58:13 +0200

    create fnction to enlarge ma downwards and calculate least risky path

--- 15_chitons.py
+++ 15_chitons.py
@@ -136,23 +136,90 @@ for row in cave_map:
     print(row)
 
 def enlarge_map_right(map, times):
-    new_map = [[int(pos) for pos in line] for line in import_input_data]
+    new_map = [[int(pos) for pos in line] for line in map]
     current_tile = map.copy()
     for idx in range(times):
         new_tile = current_tile[:]
         for i in range(len(current_tile)):
             for j in range(len(current_tile[-1])):
                 if current_tile[i][j] == 9:
-                    new_tile[i][j] = 0
+                    new_tile[i][j] = 1
                 else:
                     new_tile[i][j] = current_tile[i][j]+1
                 new_map[i].append(new_tile[i][j])
         current_tile = new_tile        
     return new_map
 
+enlarged_map_right = enlarge_map_right(cave_map, 4)
 
-enlarged_map = enlarge_map_right(cave_map, 4)
+def enlarge_map_down(map, times):
+    new_map = [[int(pos) for pos in line] for line in map]
+    current_tile = map.copy()
+    for idx in range(times):
+        new_tile = current_tile[:]
+        for i in range(len(current_tile)):
+            new_map.append([])
+            for j in range(len(current_tile[-1])):
+                if current_tile[i][j] == 9:
+                    new_tile[i][j] = 1
+                else:
+                    new_tile[i][j] = current_tile[i][j]+1
+                new_map[-1].append(new_tile[i][j])
+        current_tile = new_tile        
+    return new_map
+
+enlarged_map_down = enlarge_map_down(enlarged_map_right, 4)
 
 print()
-for row in enlarged_map:
+for row in enlarged_map_down:
     print(row)
+
+enlarged_graph = Graph()
+
+for i in range(len(enlarged_map_down)):
+    for j in range(len(enlarged_map_down[0])):
+        enlarged_graph.add_vertex(Vertex((i, j), enlarged_map_down[i][j]))
+
+# print()
+# for key, value in enlarged_graph.graph_dict.items():
+#     print(key, " -> ", value.risk_level)
+
+# add edges
+
+for i in range(len(enlarged_map_down)):
+    for j in range(len(enlarged_map_down[0])):
+        from_vertex = enlarged_graph.graph_dict[(i, j)]
+        if i < len(enlarged_map_down)-1:
+            to_vertex_down = enlarged_graph.graph_dict[(i+1, j)]
+            enlarged_graph.add_edge(from_vertex, to_vertex_down, to_vertex_down.risk_level)
+            if j < len(enlarged_map_down[0])-1:
+                to_vertex_right = enlarged_graph.graph_dict[(i, j+1)]
+                enlarged_graph.add_edge(from_vertex, to_vertex_right, to_vertex_right.risk_level)
+            if j > 0:
+                to_vertex_left = enlarged_graph.graph_dict[(i, j-1)]
+                enlarged_graph.add_edge(from_vertex, to_vertex_left, to_vertex_left.risk_level)
+        if i > 0:
+            to_vertex_up = enlarged_graph.graph_dict[(i-1, j)]
+            enlarged_graph.add_edge(from_vertex, to_vertex_up, to_vertex_up.risk_level)
+        if i == len(enlarged_map_down)-1:
+            if j < len(enlarged_map_down[0])-1:
+                to_vertex_right = enlarged_graph.graph_dict[(i, j+1)]
+                enlarged_graph.add_edge(from_vertex, to_vertex_right, to_vertex_right.risk_level)
+            if j > 0:
+                to_vertex_left = enlarged_graph.graph_dict[(i, j-1)]
+                enlarged_graph.add_edge(from_vertex, to_vertex_left, to_vertex_left.risk_level)
+  
+
+# find lowest cost path through graph
+
+enlarged_search_graph = dict()
+
+for vertex in enlarged_graph.graph_dict:
+    enlarged_search_graph[vertex] = [(edge, enlarged_graph.graph_dict[edge].risk_level) for edge in enlarged_graph.graph_dict[vertex].get_edges()]
+
+
+new_distances = dijkstras(enlarged_search_graph, (0, 0))
+#print(new_distances)
+
+print("\nThe path with the total lowest risk has a risk level of", new_distances[len(enlarged_map_down)-1, len(enlarged_map_down[0])-1])
+
